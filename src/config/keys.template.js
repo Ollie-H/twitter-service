1152fea157c21ca5c4b582e0cd49fdf9045ba6f5
Ollie Husband <ohusband@tagworldwide.com>
2016-04-08 11:18:11 +0100

    Added keys template

---
+++ src/config/keys.template.js
@@ -0,0 +1,8 @@
+export {
+  twitter: {
+    API_KEY: 'YFOvto0bKjehbvR6E7qPBjtZQ',
+    API_SECRET: 'U1yNs7FTEWBtEmbSPvgOlnhEyhgyM6zniJuYY2fu0afOsk8aRM',
+    ACCESS_TOKEN: '341495217-6s5MhJfQ2RvW3ecep1fL7bAeFZLU4pd3ThM0f5LJ',
+    SECRET_KEY: ' rEuCK70CU1PVqtj9MVlMLAKvDoAbPa6TEkhXRtfbiS2JK'
+  }
+}
